2237411262b1ff73e3c434900fe559641862f409
bm3182 <bm3182@gmail.com>
2024-07-11 16:42:31 +0900

    update

--- src/scouter/plugin/server/alert/email/EmailPlugin.java
+++ src/scouter/plugin/server/alert/email/EmailPlugin.java
@@ -17,14 +17,26 @@
  */
 package scouter.plugin.server.alert.email;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.SimpleEmail;
+
 import scouter.lang.AlertLevel;
 import scouter.lang.TextTypes;
 import scouter.lang.TimeTypeEnum;
 import scouter.lang.counters.CounterConstants;
-import scouter.lang.pack.*;
+import scouter.lang.pack.AlertPack;
+import scouter.lang.pack.MapPack;
+import scouter.lang.pack.ObjectPack;
+import scouter.lang.pack.PerfCounterPack;
+import scouter.lang.pack.XLogPack;
 import scouter.lang.plugin.PluginConstants;
 import scouter.lang.plugin.annotation.ServerPlugin;
 import scouter.net.RequestCmd;
@@ -36,13 +48,6 @@ import scouter.server.db.TextRD;
 import scouter.server.netio.AgentCall;
 import scouter.util.DateUtil;
 import scouter.util.HashUtil;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Scouter server plugin to send alert via email
@@ -524,13 +529,12 @@ public class EmailPlugin {
                     long gcTime = pack.data.getLong(CounterConstants.JAVA_GC_TIME);
 
                     long heapUsedThreshold = conf.getLong("ext_plugin_heap_used_threshold", 0);
-                    long heapUsedThreshold_wise = conf.getLong("ext_plugin_wise_heap_used_threshold", 0);
-                    long heapUsedThreshold_exp = conf.getLong("ext_plugin_exp_heap_used_threshold", 0);
-                    long heapUsedThreshold_qms = conf.getLong("ext_plugin_qms_heap_used_threshold", 0);
+                    long heapUsedThreshold_8G = conf.getLong("ext_plugin_8G_heap_used_threshold", 0);
+                    long heapUsedThreshold_4G = conf.getLong("ext_plugin_4G_heap_used_threshold", 0);
                     long heapUsed = pack.data.getLong(CounterConstants.JAVA_HEAP_USED);
 
                     if("/gprtwas1/wise_prd11".equals(objName) || "/gprtwas1/wise_prd12".equals(objName) || "/gprtwas2/wise_prd21".equals(objName) || "/gprtwas2/wise_prd22".equals(objName)) {
-                        if (heapUsedThreshold_wise != 0 && heapUsed > heapUsedThreshold_wise) {
+                        if (heapUsedThreshold_8G != 0 && heapUsed > heapUsedThreshold_8G) {
                             AlertPack ap = new AlertPack();
 
                             ap.level = AlertLevel.FATAL;
@@ -542,21 +546,8 @@ public class EmailPlugin {
 
                             alert(ap);
                         }
-                    } else if("/cjwas03/expwas01".equals(objName) || "/cjwas04/expwas02".equals(objName)) {
-                        if (heapUsedThreshold_exp != 0 && heapUsed > heapUsedThreshold_exp) {
-                            AlertPack ap = new AlertPack();
-
-                            ap.level = AlertLevel.FATAL;
-                            ap.objHash = objHash;
-                            ap.title = "Heap used exceed a threshold.";
-                            ap.message = objName + " Heap uesd(" + heapUsed + " M) exceed a threshold.";
-                            ap.time = System.currentTimeMillis();
-                            ap.objType = objType;
-
-                            alert(ap);
-                        }
-                    } else if("/cjwas03/qmswas1".equals(objName) || "/cjwas04/qmswas2".equals(objName)) {
-                        if (heapUsedThreshold_qms != 0 && heapUsed > heapUsedThreshold_qms) {
+                    } else if("/cjwas03/expwas01".equals(objName) || "/cjwas04/expwas02".equals(objName) || "/cjwas03/qmswas1".equals(objName) || "/cjwas04/qmswas2".equals(objName) || "/cjwas03/amsprd_1".equals(objName) || "/cjwas04/amsprd_2".equals(objName) || "/cjwas03/cmsprd_1".equals(objName) || "/cjwas04/cmsprd_2".equals(objName) || "/cjirisap1/bmis_was1".equals(objName) || "/cjirisap1/iris_was1".equals(objName) || "/cjemap/bmis_was2".equals(objName) || "/cjemap/iris_was2".equals(objName)) {
+                        if (heapUsedThreshold_4G != 0 && heapUsed > heapUsedThreshold_4G) {
                             AlertPack ap = new AlertPack();
 
                             ap.level = AlertLevel.FATAL;
